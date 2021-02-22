c2148493498f5f59964cfe700fc688eac91cb10e
root <sathisp80-gif>
2021-02-22 20:14:01 +0000

    Added

---
+++ generate_report.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#Start lines
+
+echo "<!DOCTYPE html> <html> <head>  </head> <body>  <h1>API-CGP-REPORTS</h1>  <ol>" >index.html
+
+for i in $(echo */); 
+do 
+#echo ${i%%/}; 
+
+echo "<ul><a href="${i%%/}/overview-features.html">${i%%/}</a></ul>" >>index.html
+
+done
+
+echo "</ol>   </body> </html>" >>index.html
